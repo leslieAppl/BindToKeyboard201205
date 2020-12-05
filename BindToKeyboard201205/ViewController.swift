fa4ae633265deed4cae65eb3ed44451bfe627e76
Leslie <leslie.appl@gmail.com>
2020-12-05 13:15:08 +0800

    Bind to keyboard infrastructure

--- BindToKeyboard201205/ViewController.swift
+++ BindToKeyboard201205/ViewController.swift
@@ -11,9 +11,44 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.bindToKeyboard()
+        setupView()
     }
 
+    func setupView() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func handleTap() {
+        view.endEditing(true)
+    }
 
 }
 
+extension UIView {
+    
+    //    func bindToKeyboard(){
+    //        NotificationCenter.default.addObserver(self, selector: #selector(UIView.keyboardWillChange(_:)), name: NOTIF_KEYBOARD_WILL_CHANGE, object: nil)
+    //    }
+    
+    func bindToKeyboard(){
+        NotificationCenter.default.addObserver(self, selector: #selector(UIView.keyboardWillChange(_:)), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+    }
+    
+    @objc func keyboardWillChange(_ notification: NSNotification) {
+        let duration = notification.userInfo![UIResponder.keyboardAnimationDurationUserInfoKey] as! Double
+        let curve = notification.userInfo![UIResponder.keyboardAnimationCurveUserInfoKey] as! UInt
+        let curFrame = (notification.userInfo![UIResponder.keyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue
+        let targetFrame = (notification.userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        let deltaY = targetFrame.origin.y - curFrame.origin.y
+        
+        UIView.animateKeyframes(withDuration: duration, delay: 0.0, options: UIView.KeyframeAnimationOptions(rawValue: curve), animations: {
+            self.frame.origin.y += deltaY
+            
+        },completion: {(true) in
+            self.layoutIfNeeded()
+        })
+    }
+}
